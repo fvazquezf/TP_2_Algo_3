cdd2877626afe3130e94edfe9241315b2e1ef790
Luciana Tarzia <ltarzia@fi.uba.ar>
2020-07-22 21:36:39 -0300

    ubico parser en otra clase: LectordeArchivo

--- src/main/java/edu/fiuba/algo3/modelo/Panel.java
+++ src/main/java/edu/fiuba/algo3/modelo/Panel.java
@@ -22,36 +22,8 @@ public class Panel {
     }
 
     public void crearPreguntaVoFClasica(String archivo) {
-        String JsonTexto = null;
-        List respuestasLeidas = new ArrayList();
-        String textoPregunta = "\0";
-        boolean respuestaCorrecta = true;
-
-        try{
-            JsonTexto = new String(Files.readAllBytes(Paths.get(archivo)));
-        }
-        catch (Exception e){
-            e.printStackTrace();
-        }
-
-        JSONObject preguntasObj = new JSONObject(JsonTexto);
-
-        JSONArray todasLasPreguntas = preguntasObj.getJSONArray("Preguntas");
-
-        for(int i = 0; i < todasLasPreguntas.length(); i++){
-            JSONObject unaPregunta =  todasLasPreguntas.getJSONObject(i);
-
-            textoPregunta = unaPregunta.getString("Pregunta");
-            JSONArray respuestasPosibles = unaPregunta.getJSONArray("PosiblesRespuestas");
-            for(int j = 0; j < respuestasPosibles.length(); j++){
-                respuestasLeidas.add(respuestasPosibles.getBoolean(j));
-            }
-            respuestaCorrecta = unaPregunta.getBoolean("RespuestaCorrecta");
-
-        }
-
-        Pregunta pregunta = new Pregunta(textoPregunta,respuestaCorrecta);
-        preguntas = pregunta;
+        LectorDeArchivo lector = new LectorDeArchivo();
+        preguntas = lector.leerArchivo(archivo);
     }
 
     public boolean pasarRespuesta() {
