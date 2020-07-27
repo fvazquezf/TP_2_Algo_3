ee93b750983a0de186c70a151dbc883800fd825f
Raul <chaparroraulantonio@gmail.com>
2020-07-26 23:08:35 -0300

    Se agrega la clase Respuesta. Tener en cuenta que la todo lo que usa JSON se comentó para simplificar las pruebas. Hay que realizar un refactor en su momento.

--- src/main/java/edu/fiuba/algo3/modelo/LectorDeArchivo.java
+++ src/main/java/edu/fiuba/algo3/modelo/LectorDeArchivo.java
@@ -1,47 +1,47 @@
-package edu.fiuba.algo3.modelo;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-
-public class LectorDeArchivo {
-
-    private Pregunta preguntas;
-
-    public Pregunta leerArchivo(String archivo){
-
-        String JsonTexto = null;
-        List respuestasLeidas = new ArrayList();
-        String textoPregunta = "\0";
-        boolean respuestaCorrecta = true;
-
-        try{
-            JsonTexto = new String(Files.readAllBytes(Paths.get(archivo)));
-        }
-        catch (Exception e){
-            e.printStackTrace();
-        }
-
-        JSONObject preguntasObj = new JSONObject(JsonTexto);
-
-        JSONArray todasLasPreguntas = preguntasObj.getJSONArray("Preguntas");
-
-        for(int i = 0; i < todasLasPreguntas.length(); i++){
-            JSONObject unaPregunta =  todasLasPreguntas.getJSONObject(i);
-
-            textoPregunta = unaPregunta.getString("Pregunta");
-            JSONArray respuestasPosibles = unaPregunta.getJSONArray("PosiblesRespuestas");
-            for(int j = 0; j < respuestasPosibles.length(); j++){
-                respuestasLeidas.add(respuestasPosibles.getBoolean(j));
-            }
-            respuestaCorrecta = unaPregunta.getBoolean("RespuestaCorrecta");
-            preguntas = new Pregunta(textoPregunta,respuestaCorrecta);
-
-        }
-        return preguntas;
-    }
-}
+//package edu.fiuba.algo3.modelo;
+//
+//import org.json.JSONArray;
+//import org.json.JSONObject;
+//
+//import java.nio.file.Files;
+//import java.nio.file.Paths;
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//public class LectorDeArchivo {
+//
+//    private Pregunta preguntas;
+//
+//    public Pregunta leerArchivo(String archivo){
+//
+//        String JsonTexto = null;
+//        List respuestasLeidas = new ArrayList();
+//        String textoPregunta = "\0";
+//        boolean respuestaCorrecta = true;
+//
+//        try{
+//            JsonTexto = new String(Files.readAllBytes(Paths.get(archivo)));
+//        }
+//        catch (Exception e){
+//            e.printStackTrace();
+//        }
+//
+//        JSONObject preguntasObj = new JSONObject(JsonTexto);
+//
+//        JSONArray todasLasPreguntas = preguntasObj.getJSONArray("Preguntas");
+//
+//        for(int i = 0; i < todasLasPreguntas.length(); i++){
+//            JSONObject unaPregunta =  todasLasPreguntas.getJSONObject(i);
+//
+//            textoPregunta = unaPregunta.getString("Pregunta");
+//            JSONArray respuestasPosibles = unaPregunta.getJSONArray("PosiblesRespuestas");
+//            for(int j = 0; j < respuestasPosibles.length(); j++){
+//                respuestasLeidas.add(respuestasPosibles.getBoolean(j));
+//            }
+//            respuestaCorrecta = unaPregunta.getBoolean("RespuestaCorrecta");
+//            preguntas = new Pregunta(textoPregunta,respuestaCorrecta);
+//
+//        }
+//        return preguntas;
+//    }
+//}
--- src/main/java/edu/fiuba/algo3/modelo/Pregunta.java
+++ src/main/java/edu/fiuba/algo3/modelo/Pregunta.java
@@ -3,25 +3,24 @@ package edu.fiuba.algo3.modelo;
 import java.util.ArrayList;
 
 public class Pregunta {
-    private final boolean respuesta;
+
+    private final Respuesta respuesta;
     private final String textoPregunta;
 
-    public Pregunta(String unaPregunta, boolean unaRespuesta) {
+
+    public Pregunta(String unaPregunta, ArrayList<Boolean> unaRespuesta) {
         textoPregunta = unaPregunta;
-        respuesta = unaRespuesta;
+        respuesta = new Respuesta(unaRespuesta);
     }
 
     public String pasarPregunta() {
         return textoPregunta;
     }
 
-    public boolean pasarRespuesta() {
-        return respuesta;
-    }
 
-    public void hacerPregunta(ArrayList<Jugador> jugadores, ArrayList<Boolean> respuestasJugadores) {
+    public void hacerPregunta(ArrayList<Jugador> jugadores, ArrayList<String> respuestasJugadores) {
         for (int i = 0; i < jugadores.size(); i++){
-             jugadores.get(i).responder(respuesta, respuestasJugadores.get(i));
+            jugadores.get(i).responder(respuesta,respuestasJugadores.get(i));
         }
     }
 }
