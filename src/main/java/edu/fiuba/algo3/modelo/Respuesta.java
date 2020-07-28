7ee6bd2b2b81b1b8b80e3ff60b697f0424a44d26
fvazquezf <fvazquezf@fi.uba.ar>
2020-07-28 00:26:10 -0300

    Refactor: Se crea la interfaz Respuesta con sus respectivas clases concretas
    Se crea la interfaz Respuesta con la clase concreta RespuestaClasica y RespuestaConPenalidad

--- src/main/java/edu/fiuba/algo3/modelo/Respuesta.java
+++ src/main/java/edu/fiuba/algo3/modelo/Respuesta.java
@@ -1,31 +1,5 @@
 package edu.fiuba.algo3.modelo;
-import java.util.ArrayList;
-import java.util.HashMap;
-
 
 public interface Respuesta {
-
-    public final HashMap<String,Boolean> respuestas = new HashMap<String, Boolean>();
-
-    public Integer responder(String unaRespuesta);
-}
-
-
-public class RespuestaConPenalidad implements Respuesta {
-
-    private final HashMap<String,Boolean> respuestas = new HashMap<String, Boolean>();
-//    private final CalculadorPuntajeConPenaldiad calculadorPuntaje;
-
-    default Respuesta(Respuesta unasRespuestas){
-
-        respuestas.put("V",unasRespuestas.get(0));
-        respuestas.put("F",unasRespuestas.get(1));
-    }
-
-    default Integer responder(String unaRespuesta){
-
-        return calculadorPuntaje.calcular(respuestas.get(unaRespuesta));
-    }
-
-
+    Integer responder(String unaRespuesta);
 }
