155dc306dfb918c0b3ca0ab482f5a1d33d0169f6
Raul <chaparroraulantonio@gmail.com>
2020-08-02 19:18:36 -0300

    Se realiza las pruebas para pregunta VoF clasica

--- src/main/java/edu/fiuba/algo3/modelo/CalculadorPuntaje.java
+++
@@ -1,7 +0,0 @@
-package edu.fiuba.algo3.modelo;
-
-import java.util.Map;
-
-public interface CalculadorPuntaje {
-    Integer calcular(Map<Integer, Boolean> respuestasPregunta, Map<Integer, Boolean> respuestasJugador);
-}
