d4e04f9c79d95f6a679437d4a370021fa635eb5b
Ivan-Grzegorczyk <igrzegorczyk@fi.uba.ar>
2020-08-08 21:55:22 -0300

    Se elimina código comentado

--- src/main/java/edu/fiuba/algo3/modelo/multiplicadores/Triplicador.java
+++ src/main/java/edu/fiuba/algo3/modelo/multiplicadores/Triplicador.java
@@ -3,10 +3,8 @@ package edu.fiuba.algo3.modelo.multiplicadores;
 import edu.fiuba.algo3.modelo.excepciones.ExcepcionYaUsasteTuTriplicadorSalame;
 
 public class Triplicador implements Multiplicador {
-    //private final int triplicadoresDisponibles;
 
     public Triplicador(Integer triplicadoresDisponibles) {
-        //this.triplicadoresDisponibles = triplicadoresDisponibles;
         if (triplicadoresDisponibles <= 0)
             throw new ExcepcionYaUsasteTuTriplicadorSalame();
     }
