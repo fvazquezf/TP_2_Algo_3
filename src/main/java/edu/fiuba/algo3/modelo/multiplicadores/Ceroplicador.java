16d88cff486c0e5f4063fccce6619d72c404d93d
fvazquezf <fvazquezf@fi.uba.ar>
2020-08-09 02:19:49 -0300

    cambio de carpetas

--- src/main/java/edu/fiuba/algo3/modelo/multiplicadores/Ceroplicador.java
+++ src/main/java/edu/fiuba/algo3/modelo/multiplicadores/Ceroplicador.java
@@ -1,4 +1,4 @@
-package edu.fiuba.algo3.modelo;
+package edu.fiuba.algo3.modelo.multiplicadores;
 
 import edu.fiuba.algo3.modelo.multiplicadores.Multiplicador;
 import edu.fiuba.algo3.modelo.multiplicadores.Uniplicador;
