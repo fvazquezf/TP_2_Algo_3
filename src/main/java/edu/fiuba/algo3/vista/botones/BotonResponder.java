506f280a31268e8d9ec4b8e7110e950635abcba2
Ivan-Grzegorczyk <igrzegorczyk@fi.uba.ar>
2020-08-20 02:28:47 -0300

    Estandarización de código y limpieza en los botones

--- src/main/java/edu/fiuba/algo3/vista/botones/BotonResponder.java
+++ src/main/java/edu/fiuba/algo3/vista/botones/BotonResponder.java
@@ -6,7 +6,6 @@ import edu.fiuba.algo3.modelo.Panel;
 import javafx.scene.control.Button;
 
 import java.util.Collection;
-import java.util.Set;
 
 public class BotonResponder extends Button {
     public BotonResponder(Panel panel, Collection<String> respuestasElegidas, Timer timer) {
--- src/main/java/edu/fiuba/algo3/vista/botones/BotonResponderGC.java
+++ src/main/java/edu/fiuba/algo3/vista/botones/BotonResponderGC.java
@@ -2,7 +2,6 @@ package edu.fiuba.algo3.vista.botones;
 
 
 import edu.fiuba.algo3.Timer;
-import edu.fiuba.algo3.controlador.ControladorResponder;
 import edu.fiuba.algo3.controlador.ControladorResponderGC;
 import edu.fiuba.algo3.modelo.Panel;
 import javafx.scene.control.Button;
