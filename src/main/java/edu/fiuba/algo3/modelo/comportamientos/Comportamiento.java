aec9ebf683c30bfe6fbd824ada518784c443a1a8
Ivan-Grzegorczyk <igrzegorczyk@fi.uba.ar>
2020-08-20 02:14:50 -0300

    Estandarización de código y limpieza en los comportamientos

--- src/main/java/edu/fiuba/algo3/modelo/comportamientos/Comportamiento.java
+++ src/main/java/edu/fiuba/algo3/modelo/comportamientos/Comportamiento.java
@@ -1,18 +1,10 @@
 package edu.fiuba.algo3.modelo.comportamientos;
 
-import edu.fiuba.algo3.modelo.excepciones.ExcepcionSoloPreguntaConPenalidadPuedeUsarMultiplicador;
-
 import java.util.Collection;
 
 
 public interface Comportamiento {
 
-    public abstract Integer compararRespuestas(Collection<String> opcionesJugador, Collection<String> opcionesCorrectas);
+    Integer compararRespuestas(Collection<String> opcionesJugador, Collection<String> opcionesCorrectas);
 
-//    public void activarMultiplicador() {
-//        throw new ExcepcionSoloPreguntaConPenalidadPuedeUsarMultiplicador();
-//    }
-//
-//    public void activarExclusividad() {
-//    }
 }
